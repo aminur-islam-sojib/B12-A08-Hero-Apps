142ecb668e0ec11189e68e9f8ed0f7348dda1c4f
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-10-07 20:14:06 +0600

    Add Hero Header

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -4,9 +4,9 @@ import Footer from '../components/Footer';
 
 const MainLayout = () => {
   return (
-    <div className=" flex flex-col min-h-screen">
+    <div className=" flex flex-col min-h-screen inter">
       <Navbar />
-      <div className=" flex-1">
+      <div className=" flex-1 bg-gray-100">
         <Outlet />
       </div>
       <Footer />
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import HeroHeader from './HeroHeader';
+
+const Header = () => {
+  return (
+    <div>
+      <HeroHeader />
+    </div>
+  );
+};
+
+export default Header;
